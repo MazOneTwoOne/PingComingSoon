13435ffd17853b045d49fb2cc794edc73659342d
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2021-03-28 19:09:32 +0100

    hover over button and javascript for button

--- emailValidation.js
+++ emailValidation.js
@@ -1,20 +1,38 @@
-console.log("Hello World");
+function check() {
+    const MAILFORMAT = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; // apparently saw this which helps check validity of email string
+    if(!document.getElementById("subscribe-email").checkValidity(MAILFORMAT))
+      {
+        document.getElementById("subscribe-email").style.borderColor = "red";
+        showText();
+        //newTextInput();
+      } else {
+        alert("Thank you, we'll be in touch.");
+      }
+};
 
-
-/*
-function ValidateEmail(inputText){
-        var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-        if(inputText.value.match(mailformat))
-    {
-        alert("Valid email address!");
-        document.form1.text1.focus();
-        return true;
-    }
-        else
-    {
-        alert("You have entered an invalid email address!");
-        document.form1.text1.focus();
-        return false;
-    }
+function showText() {
+  var x = document.getElementById("hiddenText");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "block";
+  }
 }
+
+
+// This was used to create a new paragraph
+/*
+function newTextInput() {
+    var para = document.createElement("p");  // 'para' var assigns the styling of item 
+    var node = document.createTextNode("Please provide a valid email address");
+
+    para.appendChild(node);
+
+    var element = document.getElementById("newText");
+    element.appendChild(para);
+    para.style.marginLeft = "20px";
+    para.style.fontSize = "8px";
+    para.style.fontStyle = "italic";
+    para.style.color = "red";
+};
 */
