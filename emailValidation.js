2b2d6be636b14be41983ff32a8047af37e6925eb
MazOneTwoOne <76905544+MazOneTwoOne@users.noreply.github.com>
2021-04-07 21:39:09 +0100

    tidying Javascript file

--- emailValidation.js
+++ emailValidation.js
@@ -1,24 +1,26 @@
 function check() {
     const MAILFORMAT = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; // apparently saw this which helps check validity of email string
-    if(!document.getElementById("subscribe-email").checkValidity(MAILFORMAT))
+    let correctEmail = document.getElementById("subscribe-email"); 
+    let hiddenTextField = document.getElementById("hiddenText");
+    if(!correctEmail.checkValidity(MAILFORMAT))
       {
-        document.getElementById("subscribe-email").style.borderColor = "red";
-        showText();
+        correctEmail.style.borderColor = "red";
+        showText(hiddenTextField);
         //newTextInput();
       } else {
+        correctEmail.style.borderColor = "hsla(228, 100%, 68.75%, 0.42)";
+        hideText(hiddenTextField);
         alert("Thank you, we'll be in touch.");
       }
 };
 
-function showText() {
-  var x = document.getElementById("hiddenText");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "block";
-  }
-}
+function showText(x) {
+  return x.style.display = "block";
+};
 
+function hideText(x){
+  return x.style.display = "none";
+};
 
 // This was used to create a new paragraph
 /*
